9fcf1286d2c5d7423bb3199e4608ed51f960e90c
Tunglam-1011 <donglinyang1011@gmail.com>
2026-09-10 19:25:14 +0800

    update: config.js via Shuttle Manager API

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // 此文件由 Shuttle Manager 通过 GitHub API 远程同步与自动写入
 window.SHUTTLE_CONFIG = {
   "notice": {
-    "active": true,
-    "text": "test at 1900"
+    "active": false,
+    "text": ""
   },
   "overrides": {
     "2026-09-10": "weekend"
